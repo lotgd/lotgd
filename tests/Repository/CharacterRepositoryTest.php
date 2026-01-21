6846b80591a5aa081a323f29280e1e95cacef681
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-21 18:31:34 +0100

    Adds a missing UsesClass statement.

--- tests/Repository/CharacterRepositoryTest.php
+++ tests/Repository/CharacterRepositoryTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 namespace LotGD2\Tests\Repository;
 
 use LotGD2\Entity\Mapped\Character;
+use LotGD2\Kernel;
 use LotGD2\Repository\CharacterRepository;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 #[CoversClass(CharacterRepository::class)]
+#[UsesClass(Kernel::class)]
 class CharacterRepositoryTest extends KernelTestCase
 {
     use EntityManagerSetupTrait;
