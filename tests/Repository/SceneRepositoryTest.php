85773688096ffd5632f0424a6f6321618c1e9a75
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-21 18:31:34 +0100

    Fixes SceneRepositoryTest to explicitly define the use of the Kernel.

--- tests/Repository/SceneRepositoryTest.php
+++ tests/Repository/SceneRepositoryTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 namespace LotGD2\Tests\Repository;
 
 use LotGD2\Entity\Mapped\Scene;
+use LotGD2\Kernel;
 use LotGD2\Repository\SceneRepository;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 #[CoversClass(SceneRepository::class)]
+#[UsesClass(Kernel::class)]
 class SceneRepositoryTest extends KernelTestCase
 {
     use EntityManagerSetupTrait;
