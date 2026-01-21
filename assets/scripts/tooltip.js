47560354f3b52704f080bdb8139af9ba04d48037
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-21 18:31:33 +0100

    Adjusts the markup of the action debug information.

--- assets/scripts/tooltip.js
+++ assets/scripts/tooltip.js
@@ -1,6 +1,12 @@
 import * as bootstrap from 'bootstrap'
 
 function renderTooltips() {
+
+    const allowList = bootstrap.Tooltip.Default.allowList;
+    allowList.dl = []
+    allowList.dt = []
+    allowList.dd = []
+
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 }
