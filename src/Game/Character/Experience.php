b23b89b063c11da4fb959c02d44f6b0a07093771
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-21 18:31:33 +0100

    Adds a stub for a class handling experience and levelups.

---
+++ src/Game/Character/Experience.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD2\Game\Character;
+
+class Experience
+{
+    const string PropertyName = 'experience';
+    const string LevelPropertyName = 'level';
+}
