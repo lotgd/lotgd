bbe19ccb8f274cf48950bb9a8ba3ff261daa7557
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-21 18:31:33 +0100

    Adds a Health service to deal with a character's health.

---
+++ src/Game/Character/Health.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD2\Game\Character;
+
+use LotGD2\Entity\Mapped\Character;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+readonly class Health
+{
+    const string HealthPropertyName = 'health';
+    const string MaxHealthPropertyName = 'maxHealth';
+
+    public function __construct(
+        private LoggerInterface $logger,
+        #[Autowire(expression: "service('lotgd2.game_loop').getCharacter()")]
+        private Character $character,
+    ) {
+    }
+
+    public function getHealth(): int
+    {
+        return $this->character->getProperty(self::HealthPropertyName, 10);
+    }
+
+    public function setHealth(int $health): self
+    {
+        $this->logger->debug("{$this->character->getId()}: health set to {$health} (was {$this->getHealth()}) before).");
+        $this->character->setProperty(self::HealthPropertyName, $health);
+        return $this;
+    }
+
+    /**
+     * Increases the health of a Character by a given amount.
+     *
+     * If the health surpasses the character's max health, the health is limited to the maximum.
+     * If the health is going into negative, the character's health is set to 0 instead.
+     * @param int $health
+     * @return $this
+     */
+    public function heal(int $health): self
+    {
+        $this->logger->debug("{$this->character->getId()}: healed by {$health}.");
+        $this->character->setProperty(
+            self::HealthPropertyName,
+            min(
+                $this->character->getProperty(self::MaxHealthPropertyName),
+                max(
+                    $this->getHealth() + $health,
+                    0
+                )
+            )
+        );
+        return $this;
+    }
+
+    public function getMaxHealth(): int
+    {
+        return $this->character->getProperty(self::MaxHealthPropertyName, 10);
+    }
+
+    public function setMaxHealth(int $maxHealth): self
+    {
+        $this->logger->debug("{$this->character->getId()}: health set to {$maxHealth} (was {$this->getMaxHealth()}) before).");
+        $this->character->setProperty(self::MaxHealthPropertyName, $maxHealth);
+        return $this;
+    }
+
+    public function isAlive(): bool
+    {
+        return $this->character->getProperty(self::HealthPropertyName) > 0;
+    }
+}
---
+++ tests/Game/Character/HealthTest.php
@@ -0,0 +1,145 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD2\Tests\Game\Character;
+
+use LotGD2\Entity\Mapped\Character;
+use LotGD2\Game\Character\Health;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+#[CoversClass(Health::class)]
+#[UsesClass(Character::class)]
+final class HealthTest extends TestCase
+{
+    public static function getHealthProvider(): array
+    {
+        return [
+            [0],
+            [10],
+        ];
+    }
+
+    #[DataProvider("getHealthProvider")]
+    public function testGetHealth(int $isHealth): void
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::HealthPropertyName => $isHealth,
+        ]);
+
+        $health = new Health($this->createMock(LoggerInterface::class), $character);
+
+        $this->assertEquals($isHealth, $health->getHealth());
+    }
+
+
+    public static function setHealthProvider(): array
+    {
+        return [
+            [0, 5, 5],
+            [10, 20, 20],
+        ];
+    }
+
+    #[DataProvider("setHealthProvider")]
+    public function testSetHealth(int $isHealth, int $setHealth, int $isNewHealth): void
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::HealthPropertyName => $isHealth,
+        ]);
+
+        $loggerMock = $this->createMock(LoggerInterface::class);
+        $loggerMock->expects($this->once())->method("debug");
+
+        $health = new Health($loggerMock, $character);
+
+        $health->setHealth($setHealth);
+
+        $this->assertEquals($isNewHealth, $health->getHealth());
+    }
+
+
+    public static function healProvider(): array
+    {
+        return [
+            [10, 20, 5, 15],
+            [10, 20, 10, 20],
+            [10, 20, 15, 20],
+            [10, 20, -5, 5],
+            [10, 20, -10, 0],
+            [10, 20, -15, 0],
+        ];
+    }
+
+    #[DataProvider("healProvider")]
+    public function testHeal(int $isHealth, int $isMaxHealth, int $healAmount, int $newHealth): void
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::HealthPropertyName => $isHealth,
+            Health::MaxHealthPropertyName => $isMaxHealth,
+        ]);
+
+        $health = new Health($this->createMock(LoggerInterface::class), $character);
+
+        $health->heal( $healAmount);
+        $this->assertEquals($newHealth, $health->getHealth());
+    }
+
+    #[DataProvider("getHealthProvider")]
+    public function testGetMaxHealth(int $isHealth): void
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::MaxHealthPropertyName => $isHealth,
+        ]);
+
+        $health = new Health($this->createMock(LoggerInterface::class), $character);
+
+        $this->assertEquals($isHealth, $health->getMaxHealth());
+    }
+
+    #[DataProvider("setHealthProvider")]
+    public function testSetMaxHealth(int $isMaxHealth, int $setMaxHealth, int $isNewMaxHealth): void
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::MaxHealthPropertyName => $isMaxHealth,
+        ]);
+
+        $loggerMock = $this->createMock(LoggerInterface::class);
+        $loggerMock->expects($this->once())->method("debug");
+
+        $health = new Health($loggerMock, $character);
+
+        $health->setMaxHealth($setMaxHealth);
+
+        $this->assertEquals($isNewMaxHealth, $health->getMaxHealth());
+    }
+
+    public static function isAliveProvider(): array
+    {
+        return [
+            "Is Alive" => [10, true],
+            "Is Dead" => [0, false],
+        ];
+    }
+
+    #[DataProvider("isAliveProvider")]
+    public function testIsAlive(int $setHealth, bool $aliveStatus)
+    {
+        $character = new Character();
+        $character->setProperties([
+            Health::HealthPropertyName => $setHealth,
+        ]);
+
+        $health = new Health($this->createMock(LoggerInterface::class), $character);
+
+        $this->assertEquals($aliveStatus, $health->isAlive());
+    }
+}
