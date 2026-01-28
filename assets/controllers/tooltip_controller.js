75b0266fd58e8ee88455190ee52661995b059487
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-28 17:02:15 +0100

    Improvements upon the responsive layout.
    - Small screens have now a scroll spy on the navigation sidebar coupled to a script in tooltip_controller.js.
    - XXL screens use now a 2, 8, 2 column layout
    - Adding w-100 classes to force layout to 100% width

--- assets/controllers/tooltip_controller.js
+++ assets/controllers/tooltip_controller.js
@@ -2,6 +2,7 @@
 import { Controller } from '@hotwired/stimulus';
 import { getComponent } from '@symfony/ux-live-component';
 import { renderTooltips } from '../scripts/tooltip';
+import * as bootstrap from 'bootstrap'
 
 export default class extends Controller {
     async initialize() {
@@ -9,6 +10,14 @@ export default class extends Controller {
 
         this.component.on('render:finished', (component) => {
             renderTooltips();
+            this.refreshScrollSpy();
+        });
+    }
+
+    refreshScrollSpy()
+    {
+        const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+            target: this.scrollSpyTarget
         });
     }
 }
