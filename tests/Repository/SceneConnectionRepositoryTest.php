5a4a04831bc0773cec0d9b70e20829d6c9f261b3
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-31 09:32:51 +0100

    Fixes errors raised by PHPUnit

--- tests/Repository/SceneConnectionRepositoryTest.php
+++ tests/Repository/SceneConnectionRepositoryTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 namespace LotGD2\Tests\Repository;
 
 use LotGD2\Entity\Mapped\SceneConnection;
+use LotGD2\Kernel;
 use LotGD2\Repository\SceneConnectionRepository;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 #[CoversClass(SceneConnectionRepository::class)]
+#[UsesClass(Kernel::class)]
 class SceneConnectionRepositoryTest extends KernelTestCase
 {
     use EntityManagerSetupTrait;
