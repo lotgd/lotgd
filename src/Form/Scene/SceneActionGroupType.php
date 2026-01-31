fa750eed96b4d9a22a33734474c1968ddebe9757
Basilius Sauter <basilius.sauter@gmail.com>
2026-01-31 08:53:00 +0100

    Improvements of the Scene Editor
    - A click on a link between two nodes inside the scene editor now opens a form to change the connection.
    - Changing the SceneActionGroup is not fully implemented yet.

--- src/Form/Scene/SceneActionGroupType.php
+++ src/Form/Scene/SceneActionGroupType.php
@@ -1,11 +1,10 @@
 <?php
 declare(strict_types=1);
 
-namespace LotGD2\Form;
+namespace LotGD2\Form\Scene;
 
 use LotGD2\Entity\Mapped\SceneActionGroup;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
