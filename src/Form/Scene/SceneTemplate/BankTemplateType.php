406e527a0d7eddd2baeabae213f416610fe59955
Basilius Sauter <basilius.sauter@gmail.com>
2026-02-13 08:25:18 +0100

    Adds more options to the BankTemplate and a new day procedure.
    - New settings for Banks: each Bank can define their minInterest, maxInterest and maxGoldInBank, as well as a turnsLeftBeforeInterest to prevent payouts for lazy characters
    - The new day procedure is executed for all account names only once, with priority given to the scene with the lower id. This is not ideal and I will create an issue to address this. I currently think to be able to reference another branch directly should solve that.

--- src/Form/Scene/SceneTemplate/BankTemplateType.php
+++ src/Form/Scene/SceneTemplate/BankTemplateType.php
@@ -7,12 +7,13 @@ use LotGD2\Form\GroupedFormType;
 use LotGD2\Form\TypeProvidesDefaultDataInterface;
 use LotGD2\Game\Scene\SceneTemplate\BankTemplate;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 use Symfony\Component\Validator\Constraints\Valid;
 
 /**
@@ -44,12 +45,48 @@ class BankTemplateType extends AbstractType implements TypeProvidesDefaultDataIn
                 "data" => $defaultData["tellerName"],
             ])
             ->add("accountName", TextType::class, [
-                "help" => "The account identifier. Choosing a different account names across scenes enables independent bank accounts.",
+                "help" => "The account identifier. Choosing a different account names across scenes enables independent bank accounts.
+                    Be careful with bank-account specific settings (such as the interest rate). If multiple apply, only the one
+                    from the Scene with the lower ID will be used.",
                 "required" => false,
                 "constraints" => [
                     new NotBlank(),
                 ],
                 "data" => $defaultData["accountName"],
+            ])
+            ->add("minInterest", IntegerType::class, [
+                "label" => "Minimum interest rate in %",
+                "required" => true,
+                "constraints" => [
+                    new Range(min: 0),
+                ],
+                "data" => $defaultData["minInterest"],
+            ])
+            ->add("maxInterest", IntegerType::class, [
+                "label" => "Maximum interest rate in %",
+                "required" => true,
+                "constraints" => [
+                    new Range(min: 0),
+                ],
+                "data" => $defaultData["maxInterest"],
+            ])
+            ->add("maxGoldInBank", IntegerType::class, [
+                "label" => "Maximum gold in bank before no interest is paid out.",
+                "help" => "Set to 0 to disable. Prevents that rich people get too rich",
+                "required" => true,
+                "constraints" => [
+                    new Range(min: 0),
+                ],
+                "data" => $defaultData["maxGoldInBank"],
+            ])
+            ->add("turnsLeftBeforeInterest", IntegerType::class, [
+                "label" => "Maximum amount of turns before interest is paid out.",
+                "help" => "Maximum amount of turns that a character can have left from the old day before interest is paid out. At 0, character must have exhausted all of their turns. -1 disables this feature.",
+                "required" => true,
+                "constraints" => [
+                    new Range(min: -1),
+                ],
+                "data" => $defaultData["maxGoldInBank"],
             ])
             ->add(
                 $builder->create("text", GroupedFormType::class, options: [
@@ -89,6 +126,10 @@ class BankTemplateType extends AbstractType implements TypeProvidesDefaultDataIn
         return [
             "tellerName" => "Elessa",
             "accountName" => "default",
+            "minInterest" => 1,
+            "maxInterest" => 10,
+            "maxGoldInBank" => 10000,
+            "turnsLeftBeforeInterest" => 4,
             "text" => [
                 "deposit" => <<<TXT
                     {{ tellerName }} records your deposit of {{ amount }} gold in her ledger.
--- src/Game/Scene/SceneTemplate/BankTemplate.php
+++ src/Game/Scene/SceneTemplate/BankTemplate.php
@@ -8,21 +8,31 @@ use LotGD2\Entity\Action;
 use LotGD2\Entity\Mapped\Character;
 use LotGD2\Entity\Mapped\Scene;
 use LotGD2\Entity\Mapped\Stage;
+use LotGD2\Event\StageChangeEvent;
 use LotGD2\Form\Scene\SceneTemplate\BankTemplateType;
 use LotGD2\Game\Character\Gold;
+use LotGD2\Game\Character\Health;
+use LotGD2\Game\GameTime\NewDay;
+use LotGD2\Game\Random\DiceBagInterface;
 use LotGD2\Game\Scene\SceneAttachment\SimpleFormAttachment;
 use LotGD2\Game\Stage\ActionService;
 use LotGD2\Repository\AttachmentRepository;
+use LotGD2\Repository\SceneRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 /**
  * @phpstan-type BankTemplateConfiguration array{
  *     tellerName: string,
  *     accountName: string,
+ *     minInterest: int,
+ *     maxInterest: int,
+ *     maxGoldInBank: int,
+ *     turnsLeftBeforeInterest: int,
  *     text: array{
  *         deposit: string,
  *         withdraw: string,
@@ -38,9 +48,12 @@ readonly class BankTemplate implements SceneTemplateInterface
 
     public function __construct(
         private LoggerInterface $logger,
-        private Gold $gold,
+        private Stopwatch $stopwatch,
         private AttachmentRepository $attachmentRepository,
+        private SceneRepository $sceneRepository,
+        private DiceBagInterface $diceBag,
         private ActionService $actionService,
+        private Gold $gold,
     ) {
     }
 
@@ -170,5 +183,68 @@ readonly class BankTemplate implements SceneTemplateInterface
     private function addGoldInBank(Character $character, array $templateConfig, int $amount): void
     {
         $this->setGoldInBank($character, $templateConfig, $this->getGoldInBank($character, $templateConfig) + $amount);
+    }
+
+    #[AsEventListener(event: NewDay::PostNewDay)]
+    public function onNewDayEvent(StageChangeEvent $event): void
+    {
+        $this->stopwatch->start("lotgd2.BankTemplate.onNewDayEvent");
+        $defaultConfig = new BankTemplateType()->getDefaultData();
+
+        $oldHealth = new Health(null, $event->characterBefore);
+
+        $scenes = $this->sceneRepository->findBy(["templateClass" => self::class], ["id" => "ASC"]);
+        $bankAccounts = [];
+        foreach ($scenes as $scene) {
+            $config = $scene->templateConfig;
+
+            $bankAccount = $config["accountName"] ?? "default";
+            if (isset($bankAccounts[$bankAccount])) {
+                continue;
+            }
+
+            $bankAccounts[$bankAccount] = true;
+
+            $i = $scene->id;
+
+            $minInterest = $config["minInterest"] ?? $defaultConfig["minInterest"] ?? 0;
+            $maxInterest = $config["maxInterest"] ?? $defaultConfig["maxInterest"] ?? 0;
+            $maxGoldInBank = $config["maxGoldInBank"] ?? 10_000;
+            $turnsLeftBeforeInterest = $config["turnsLeftBeforeInterest"] ?? 4;
+
+            $interestRate = $this->diceBag->pseudoBell((int)round($minInterest), (int)round($maxInterest)) / 100;
+            $goldInBank = $this->getGoldInBank($event->character, $config);
+            $interest = (int)round($interestRate * $goldInBank);
+
+            $event->stage->addContext("bankName{$i}", $scene->title);
+            $event->stage->addContext("bankInterestRate{$i}", $interestRate*100);
+            $event->stage->addContext("bankInterest{$i}", $interest);
+
+            if ($goldInBank < 0) {
+                // Character has dept
+                if ($goldInBank < -$maxGoldInBank) {
+                    // Character owes a dept larger than the limit; let's not bankrupt him further.
+                    $interest = 0;
+                    $event->stage->addDescription("The bank <.{{ bankName{$i} }}.> forgoes their right to collect interest on your dept. You owe enough money already.");
+                } else {
+                    $event->stage->addDescription("The bank <.{{ bankName{$i} }}.>'s interest rate today is {{ bankInterestRate{$i} }}%. Your dept increases by an additional {{ bankInterest{$i}|abs }} gold.");
+                }
+            } elseif ($goldInBank > 0) {
+                // Character has money
+                if ($goldInBank > $maxGoldInBank) {
+                    // Character has more gold than the maximum; the bank does not pay out interest.
+                    $interest = 0;
+                    $event->stage->addDescription("The bank <.{{ bankName{$i} }}.> does not pay our your interest to retain solvency. You have already enough savings.");
+                } elseif ($turnsLeftBeforeInterest >= 0 && $turnsLeftBeforeInterest <= $oldHealth->getTurns()) {
+                    $event->stage->addDescription("The bank <.{{ bankName{$i} }}.>'s interest rate today is {{ bankInterestRate{$i} }}%, but you will not earn interest. This bank only gives interest to those who work.");
+                } else {
+                    $event->stage->addDescription("The bank <.{{ bankName{$i} }}.>'s interest rate today is {{ bankInterestRate{$i} }}%. You earned {{ bankInterest{$i}|abs }} gold interest.");
+                }
+            }
+
+            $this->addGoldInBank($event->character, $config, $interest);
+        }
+
+        $this->stopwatch->stop("lotgd2.BankTemplate.onNewDayEvent");
     }
 }
